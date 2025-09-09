c9beecd1339d1fba34806ea2e67ebda501cf8bf2
gagan-2803 <gagansharmaofficial28@gmail.com>
2025-09-09 22:08:33 +0530

    Counter App with React

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 50
+
+  const addValue = () => {
+    // counter = counter + 1;
+    // setCounter(counter)
+    // console.log("value added", counter);
+    setCounter(counter + 1)
+    setCounter(counter + 1)
+    setCounter(counter + 1)
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    if (counter > 0)
+      setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Increase Value: {counter}</button>
+      <br />
+      <button onClick={removeValue}>Decrease Value : {counter}</button>
+    </>
+  )
+}
+
+export default App
